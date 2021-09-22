ab02a0cfb0b13ba9257eb4d231af781006f8ba59
Joao Roque <jnroque@hotmail.com>
2021-09-22 20:44:48 -0300

    Ecluido uma linhado codigo

--- src/aluraExrcicios/ex001.java
+++ src/aluraExrcicios/ex001.java
@@ -1,11 +1,10 @@
 package aluraExrcicios;
 
-import com.sun.org.apache.xalan.internal.xsltc.compiler.sym;
 
 public class ex001 {
 
 	public static void main(String[] args) {
 	System.out.println("Deu Certo");
-	System.out.println("Mais uma linha ");
+	
 	}
 }
