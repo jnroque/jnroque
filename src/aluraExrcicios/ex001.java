18c6f1e2ed47e993fef46d5bf0cfcedd76e7d6ad
Joao Roque <jnroque@hotmail.com>
2021-10-05 17:45:54 -0300

    adicionei outa linha

--- src/aluraExrcicios/ex001.java
+++ src/aluraExrcicios/ex001.java
@@ -6,5 +6,6 @@ public class ex001 {
 	public static void main(String[] args) {
 	System.out.println("Deu Certo");
 	System.out.println(" Mais uma linha");
+	System.out.println("Deu Certo mais uma linha");
 	}
 }
