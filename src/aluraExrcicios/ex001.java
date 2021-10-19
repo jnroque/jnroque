ada8062fec122d0e31c6622c1c96e7c0ef4b8d67
Joao Roque <jnroque@hotmail.com>
2021-10-19 19:49:39 -0300

    mudança para vc code

--- src/aluraExrcicios/ex001.java
+++ src/aluraExrcicios/ex001.java
@@ -5,7 +5,7 @@ public class ex001 {
 
 	public static void main(String[] args) {
 	System.out.println("Deu Certo");
-	System.out.println(" Mais uma linha");
-	System.out.println("Deu Certo mais uma linha");
+	System.out.println("Mais uma linha");
+	System.out.println("Deu Certo mais uma linha ");
 	}
 }
